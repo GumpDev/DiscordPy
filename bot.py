7012fe83acf308eeedc0c0259984866f3baa3b07
Gustavo Maia Paes <gump.flash@gmail.com>
2021-03-07 15:21:31 -0300

    first commit

---
+++ bot.py
@@ -0,0 +1,27 @@
+import discord
+from dotenv import load_dotenv
+import os
+import logging
+
+load_dotenv()
+client = discord.Client()
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+@client.event
+async def on_ready():
+    print('Você está logado como {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content == '!ping':
+        await message.channel.send('O bot está com {0}ms'.format(client.latency));
+
+client.run(os.getenv('TOKEN'))
